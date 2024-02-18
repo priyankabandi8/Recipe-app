81077e212a03323687c7888bf9a0311550cb95e4
priyankabandi8 <159551084+priyankabandi8@users.noreply.github.com>
2024-02-18 21:40:53 +0530

    Add files via upload

---
+++ src/Products.js
@@ -0,0 +1,24 @@
+import React from 'react'
+const Products = ({ data }) => {
+    return (
+        <div>
+            <div className="row">
+                {data.map(data =>
+                    <div className="col-md-4">
+                        <div class="card" style={{ "width": "18rem", "Height": "100vh" }}>
+                            <img class="card-img-top" src={data.recipe.image} alt="Card image cap" />
+                            <div class="card-body">
+                            <center>
+                                <h5 class="card-title">{data.recipe.label}</h5>
+                                <p class="card-text">Total Amount Of Calories : {Math.round(data.recipe.calories)}</p>
+                                <a href="#" class="btn btn-primary">Buy</a>
+                            </center>
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </div>
+        </div>
+    )
+}
+export default Products
